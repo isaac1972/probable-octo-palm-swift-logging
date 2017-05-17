c47b4572b60ca11f1da46df99eae609d572b1e25
isaac rallo <isaacrallo@me.com>
2017-05-17 23:13:07 +0200

    Initial Commit

---
+++ probable-octo-palm-swift-logging/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  probable-octo-palm-swift-logging
+//
+//  Created by isaac rallo on 17/05/17.
+//  Copyright © 2017 isaac rallo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ probable-octo-palm-swift-loggingTests/probable_octo_palm_swift_loggingTests.swift
@@ -0,0 +1,36 @@
+//
+//  probable_octo_palm_swift_loggingTests.swift
+//  probable-octo-palm-swift-loggingTests
+//
+//  Created by isaac rallo on 17/05/17.
+//  Copyright © 2017 isaac rallo. All rights reserved.
+//
+
+import XCTest
+@testable import probable_octo_palm_swift_logging
+
+class probable_octo_palm_swift_loggingTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
